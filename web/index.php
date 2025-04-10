8c1502d244486c0109a67966ad02d323c251dad0
jordi1998 <jordicam14@gmail.com>
2025-04-10 09:03:20 +0200

    Corregit: Només es pot accedir des de localhost i ofuscació de les direccions

--- web/index.php
+++ web/index.php
@@ -10,7 +10,7 @@ $result = $mysqli->query("SELECT * FROM personas");
 echo "<h2>Personas</h2>";
 while ($row = $result->fetch_assoc()) {
     echo "Nombre: " . $row["nombre"] . ", ";
-    echo "Dirección: " . $row["direccion"] . ", ";
+    echo "Dirección: " . substr($row["direccion"], 0, 5) . "*****, ";
     echo "DNI: " . substr($row["dni"], 0, 3) . "*****, ";
     echo "Teléfono: " . substr($row["telefono"], 0, 3) . "****<br>";
 }
